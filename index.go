881ea58fd0a69d8992528b087bf963578c126b11
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-09-15 11:31:57 -0300

    Adicionando Types e Length

--- index.go
+++ index.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	moduloMath "math" // moduloMath é um Alias, opcional
+	"reflect"
 )
 
 func main() {
@@ -14,4 +15,7 @@ func main() {
 	var n1, n2, n3, n4 = 7, 8, 9, 10
 	media := (n1 + n2 + n3 + n4) / 4
 	fmt.Println("A média Final é", media)
+
+	nome := `Renan Augusto`
+	fmt.Println("A variável nome é do tipo", reflect.TypeOf(nome), "e tem", len(nome), "caracteres")
 }
