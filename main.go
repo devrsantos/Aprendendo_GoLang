7fd75cc74029ab6f93474fed8c3c7b8b799aeec0
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-09-21 16:11:42 -0300

    Aprendendo sobre Modulos

--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
+	"devrsantos/media"
+
 	"fmt"
-	"math"
-	"reflect"
 )
 
 func main() {
-	const Pi = 3.1415
-	var raio = 3.2
-	area := Pi * math.Pow(raio, 2)
-	fmt.Println("A área é", area)
-
-	FuncBasicas()
-	FuncMath()
-
-	nome := `Renan Augusto`
-	fmt.Println("A variável nome é do tipo", reflect.TypeOf(nome), "e tem", len(nome), "caracteres")
+	fmt.Println(media.Oi())
+	fmt.Println("O Conceito final é de:", media.CalculaNota(5.0, 6.0, 3.0, 5.8))
 }
+
