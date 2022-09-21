ecd3949557ec2b5ccd9d031a316f99edbbb13dee
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-09-21 16:51:06 -0300

    Utilizando Multiplos Pacotes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module myModules
 
 go 1.19
 
-require github.com/devrsantos/golang_calcula_media v0.0.0-20220921135456-e16faf6be361
+require (
+	github.com/devrsantos/pacotes_go/calculaMedia v0.0.0-20220921192736-6720ca4068b7
+	github.com/devrsantos/pacotes_go/funcaoAritimeticas v0.0.0-20220921194407-ac72216fb6f6
+)
--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	//C:\Program Files\Go\src\devrsantos\pacote_local
 	"devrsantos/pacote_local"
 	
-	"github.com/devrsantos/golang_calcula_media"
+	"github.com/devrsantos/pacotes_go/calculaMedia"
+
+	"github.com/devrsantos/pacotes_go/funcaoAritimeticas"
 
 	"fmt"
 )
@@ -12,5 +14,6 @@ import (
 func main() {
 	fmt.Println("Pacote Local: O Conceito final é de:", pacoteCalculaMedia.CalculaNota(5.0, 6.0, 3.0, 5.8))
 	fmt.Println("Pacote Remote: O Conceito final é de:", media.CalculaNota(5.0, 6.0, 3.0, 5.8))
+	funcaoAritimeticas.Aritimeticas()
 }
 
