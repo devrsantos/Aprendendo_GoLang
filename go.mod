b9aeaaae69e01f9efff1ad413b4f021e49007894
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-09-21 16:12:27 -0300

    Aprendendo Sobre Modulos

---
+++ go.mod
@@ -0,0 +1,5 @@
+module myModules
+
+go 1.19
+
+require github.com/devrsantos/golang_calcula_media v0.0.0-20220921135456-e16faf6be361
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"devrsantos/media"
+	"devrsantos/pacote_local"
+	
+	"github.com/devrsantos/golang_calcula_media"
 
 	"fmt"
 )
 
 func main() {
-	fmt.Println(media.Oi())
-	fmt.Println("O Conceito final é de:", media.CalculaNota(5.0, 6.0, 3.0, 5.8))
+	fmt.Println("Pacote Local: O Conceito final é de:", pacoteCalculaMedia.CalculaNota(5.0, 6.0, 3.0, 5.8))
+	fmt.Println("Pacote Remote: O Conceito final é de:", media.CalculaNota(5.0, 6.0, 3.0, 5.8))
 }
 
